2b50ae6aca639954d2424a4e287def5dac9f1ae7
lorenak <lorenak@gmail.com>
2011-10-17 09:28:00 +0000

    Me falta probar algunos metodos y agregarle un campo

---
+++ TP1_v2/src/ABMs/ABMDistrito.h
@@ -0,0 +1,35 @@
+/*
+ * ABMCandidato.h
+ *
+ *  Created on: 10/10/2011
+ *      Author: loo
+ */
+
+#ifndef ABMDISTRITO_H_
+#define ABMDISTRITO_H_
+
+#include "../Common/Common.h"
+#include "../Entidades/Distrito.h"
+#include "../BPlusTree/BPlusTree.h"
+#include "../utils/ConfigFile.h"
+
+class ABMDistrito {
+
+	public:
+		ABMDistrito(string bpTreeFile);
+		virtual ~ABMDistrito();
+		bool Exists(Distrito distrito);
+		bool ExistsKey(Key idDistrito);
+		void Add(Distrito distrito);
+		bool Delete(int idDistrito);
+		void Modify(Distrito distrito);
+		vector<Distrito> GetDistritos();
+		Distrito * GetDistrito(int idDistrito);
+		void mostrarDistritosPorPantalla();
+	private:
+			string bpTreeFile;
+			BPlusTree* bpPlusTree;
+
+};
+
+#endif /* ABMDISTRITO_H_ */
